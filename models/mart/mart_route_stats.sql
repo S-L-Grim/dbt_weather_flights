ac48785480ae6e87be08770806f760b848651c63
S-L-Grim <sergei.licht@yahoo.com>
2024-10-16 09:28:47 +0200

    mart new

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -1,27 +1,35 @@
-WITH route_stats AS (
-    SELECT 
-        origin AS origin_faa,
-        dest AS dest_faa,
-        COUNT(flight_number) AS total_flights,
-        COUNT(DISTINCT tail_number) AS unique_airplanes,
-        COUNT(DISTINCT airline) AS unique_airlines,
-        AVG(actual_elapsed_time) AS avg_elapsed_time,
-        AVG(arr_delay) AS avg_arrival_delay,
-        MAX(arr_delay) AS max_delay,
-        MIN(arr_delay) AS min_delay,
-        SUM(cancelled) AS total_cancelled,
-        SUM(diverted) AS total_diverted
-    FROM prep_flights
-    GROUP BY origin, dest
+WITH flights_stats AS (
+	SELECT -- TO_CHAR(flight_date, 'YYYY-MM') AS flight_month, --for the alternative GROUPBY also by month (see below)
+		   origin
+		   ,dest
+        --    ,origin || ' - ' || dest AS route -- optional (challenge: keep only 'route' in the final query output, w/o 'origin' and 'dest')
+		   ,COUNT(flight_number) AS n_flights
+		   ,COUNT(DISTINCT tail_number) AS nunique_tails
+		   ,COUNT(DISTINCT airline) AS nunique_airlines
+		   ,AVG(actual_elapsed_time)::INTEGER * ('1 second'::INTERVAL) AS avg_actual_elapsed_time 
+        --    ,(AVG(actual_elapsed_time)*60)::INTEGER * ('1 second'::INTERVAL) AS avg_actual_elapsed_time -- without rounding the seconds
+		--    ,STDDEV(actual_elapsed_time)::INTEGER * ('1 second'::INTERVAL) AS sd_actual_elapsed_time 
+		   ,AVG(arr_delay)::INTEGER * ('1 second'::INTERVAL) AS avg_arr_delay
+		   ,MIN(arr_delay_interval) AS min_arr_delay
+		   ,MAX(arr_delay_interval) AS max_arr_delay
+		   ,SUM(cancelled) AS total_cancelled
+		   ,SUM(diverted) AS total_diverted
+	FROM {{ref('prep_flights')}}
+    GROUP BY (origin, dest) -- over all time
+	-- GROUP BY (flight_month, origin, dest) -- alternative GROUPBY also by month
+),
+add_names AS (
+	SELECT o.city AS origin_city
+			,d.city AS dest_city
+			,o.name AS origin_name
+			,d.name AS dest_name
+			,f.*
+	FROM flights_stats f
+	LEFT JOIN {{ref('prep_airports')}} o
+		ON origin=o.faa
+	LEFT JOIN {{ref('prep_airports')}} d
+		ON dest=d.faa
 )
-SELECT 
-    rs.*,
-    origin_airports.city AS origin_city,
-    origin_airports.country AS origin_country,
-    origin_airports.name AS origin_name,
-    dest_airports.city AS dest_city,
-    dest_airports.country AS dest_country,
-    dest_airports.name AS dest_name
-FROM route_stats rs
-JOIN prep_airports origin_airports ON rs.origin_faa = origin_airports.faa
-JOIN prep_airports dest_airports ON rs.dest_faa = dest_airports.faa
+SELECT *
+FROM add_names
+ORDER BY (origin, dest) DESC
--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -1,31 +1,59 @@
-WITH flight_weather AS (
-    SELECT 
-        f.origin AS faa,
-        f.flight_date,
-        COUNT(DISTINCT f.flight_number) AS nunique_departures,
-        COUNT(DISTINCT f.flight_number) AS nunique_arrivals,
-        COUNT(f.sched_dep_time) AS planned_departures,
-        COUNT(f.sched_arr_time) AS planned_arrivals,
-        SUM(f.cancelled) AS total_cancelled,
-        SUM(f.diverted) AS total_diverted,
-        COUNT(f.arr_time) AS actual_departures,
-        COUNT(f.arr_time) AS actual_arrivals,
-        AVG(w.avg_temp_c) AS daily_avg_temp,
-        MAX(w.max_temp_c) AS daily_max_temp,
-        MIN(w.min_temp_c) AS daily_min_temp,
-        SUM(w.precipitation_mm) AS daily_precipitation,
-        SUM(w.snow_mm) AS daily_snowfall,
-        AVG(w.avg_wind_direction) AS avg_wind_direction,
-        AVG(w.avg_wind_speed_kmh) AS avg_wind_speed,
-        MAX(w.wind_peakgust_kmh) AS peak_wind_gust
-    FROM prep_flights f
-    JOIN prep_weather_daily w ON f.flight_date = w.date
-    GROUP BY f.origin, f.flight_date
+WITH departures AS ( 
+	SELECT flight_date 
+			,origin AS faa
+			,COUNT(DISTINCT dest) AS nunique_to
+			,COUNT(sched_dep_time) AS dep_planned
+			,SUM(cancelled) AS dep_cancelled
+			,SUM(diverted) AS dep_diverted
+			,COUNT(arr_time) AS dep_n_flights
+--			,COUNT(DISTINCT tail_number) AS dep_nunique_tails -- BONUS TASK
+--			,COUNT(DISTINCT airline) AS dep_nunique_airlines -- BONUS TASK
+	FROM {{ref('prep_flights')}}
+	WHERE origin IN (SELECT DISTINCT airport_code FROM {{ref('prep_weather_daily')}})
+	GROUP BY origin, flight_date 
+	ORDER BY origin, flight_date
+),
+arrivals AS (
+	SELECT flight_date
+			,dest AS faa
+			,COUNT(DISTINCT origin) AS nunique_from
+			,COUNT(sched_dep_time) AS arr_planned
+			,SUM(cancelled) AS arr_cancelled
+			,SUM(diverted) AS arr_diverted
+			,COUNT(arr_time) AS arr_n_flights
+--			,COUNT(DISTINCT tail_number) AS arr_nunique_tails -- BONUS TASK
+--			,COUNT(DISTINCT airline) AS arr_nunique_airlines -- BONUS TASK
+	FROM {{ref('prep_flights')}}
+	WHERE dest IN (SELECT DISTINCT airport_code FROM {{ref('prep_weather_daily')}})
+	GROUP BY dest, flight_date
+	ORDER BY dest, flight_date
+),
+total_stats AS (
+	SELECT flight_date
+			,faa
+			,nunique_to
+			,nunique_from
+	--		,(nunique_to + nunique_from)::NUMERIC/2 AS n_connections -- fractions would indicate that for the number of connections to!=from --BONUS task
+			,dep_planned + arr_planned AS total_planned
+			,dep_cancelled + arr_cancelled AS total_cancelled
+			,dep_diverted + arr_diverted AS total_diverted
+	--		,((dep_cancelled + arr_cancelled + dep_diverted + arr_diverted)::NUMERIC/(dep_planned + arr_planned)::NUMERIC)*100 AS percent_change -- BONUS TASK
+			,dep_n_flights + arr_n_flights AS total_flights
+	--		,(dep_nunique_tails + arr_nunique_tails)::NUMERIC/2 AS nunique_tails -- fractions would indicate that for the number of tails to!=from -- BONUS TASK
+	--		,(dep_nunique_airlines + arr_nunique_airlines)::NUMERIC/2 AS nunique_airlines -- fractions would indicate that for the number of airlines to!=from -- BONUS TASK
+	FROM departures
+	JOIN arrivals
+	USING (flight_date, faa)
 )
-SELECT 
-    fw.*,
-    ap.city,
-    ap.country,
-    ap.name
-FROM flight_weather fw
-JOIN prep_airports ap ON fw.faa = ap.faa
+SELECT t.* 
+		,w.min_temp_c
+		,w.max_temp_c
+		,w.precipitation_mm
+		,w.max_snow_mm
+		,w.avg_wind_direction
+		,w.avg_wind_speed_kmh
+		,w.wind_peakgust_kmh
+FROM total_stats t
+LEFT JOIN {{ref('prep_weather_daily')}} w
+ON faa = airport_code AND flight_date = date
+ORDER BY total_diverted DESC
